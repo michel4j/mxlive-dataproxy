5b2810555575d8a967f60be18fda729a3f0f8375
Michel Fodje <michel.fodje@lightsource.ca>
2019-04-08 13:44:51 -0600

    Multiple bug fixes and code cleanups. To avoid exposure archived user directories are now expected to be directly at the top level of the archive path.

--- deploy/run-server.sh
+++ deploy/run-server.sh
@@ -7,13 +7,6 @@ export SERVER_NAME=${SERVER_NAME:-$(hostname --fqdn)}
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
-# Disable chain cert if no ca.crt file available
-if [ ! -f /mxlive/local/certs/ca.crt ]; then
-    sed -i 's/    SSLCertificateChainFile/#   SSLCertificateChainFile/' /etc/httpd/conf.d/dataserver.conf
-else
-    sed -i 's/#   SSLCertificateChainFile/    SSLCertificateChainFile/' /etc/httpd/conf.d/dataserver.conf
-fi
-
 if [ ! -f /dataserver/local/.dbinit ]; then
     /dataserver/manage.py syncdb --noinput &&
     touch /dataserver/local/.dbinit
--- downloads/views.py
+++ downloads/views.py
@@ -10,6 +10,7 @@ from django.utils.decorators import method_decorator
 import os
 import posixpath
 import re
+import glob
 import subprocess
 import urllib
 from django.http import StreamingHttpResponse
@@ -46,15 +47,14 @@ class CreatePath(View):
 
 def get_download_path(key):
     """Convenience method to return a path for a key"""
-
-    obj = SecurePath.objects.get(key=key)
+    obj = get_object_or_404(SecurePath, key=key)
     return obj.path
 
 
-def send_uncompressed_file(request, obj, full_path):
-    create_cache_dir(obj.key)
+def send_uncompressed_file(request, key, full_path):
+    create_cache_dir(key)
     _, zfile = os.path.split(full_path)
-    cached_file = os.path.join(CACHE_DIR, obj.key, zfile.strip('.gz'))
+    cached_file = os.path.join(CACHE_DIR, key, zfile.strip('.gz'))
     cmd = 'gunzip {0} {1}'.format(full_path, cached_file)
     try:
         subprocess.check_call(cmd.split())
@@ -67,112 +67,103 @@ def send_raw_file(request, full_path, attachment=False):
     """Send a file using mod_xsendfile or similar functionality.
     Use django's static serve option for development servers"""
 
-    if not os.path.exists(full_path):
-        full_path = '{}{}'.format(ARCHIVE_ROOT, full_path)
+    original_path = full_path
+    archived_path = re.sub(ROOT_RE, ARCHIVE_ROOT, original_path)
 
-    if not os.path.exists(full_path):
-        logger.warning("Path not found: {}".format(full_path))
+    file_path = original_path if os.path.exists(original_path) else archived_path
+    if not os.path.exists(file_path):
+        logger.warning("Path not found: {}".format(file_path))
         raise Http404
 
     if FRONTEND == "xsendfile":
         response = HttpResponse()
-        response['X-Sendfile'] = full_path
+        response['X-Sendfile'] = file_path
         if attachment:
             response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(full_path)
+
         # Unset the Content-Type as to allow for the webserver
         # to determine it.
         response['Content-Type'] = ''
 
     elif FRONTEND == "xaccelredirect":
         response = HttpResponse()
-        response['X-Accel-Redirect'] = full_path
+        response['X-Accel-Redirect'] = file_path
+
         if attachment:
             response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(full_path)
         response['Content-Type'] = ''
-        #response = HttpResponse()
+
     else:
-        dirname = os.path.dirname(full_path)
-        path = os.path.basename(full_path)
+        dirname = os.path.dirname(file_path)
+        path = os.path.basename(file_path)
+
         # "Serving file %s in directory %s through django static serve." % (path, dirname)
         response = serve(request, path, dirname)
 
     return response
 
 
-def find_file(request, key, path):
-    obj = get_object_or_404(SecurePath, key=key)
-
-    if os.path.exists(get_download_path(obj.key)):
-        filename = os.path.join(CACHE_DIR, obj.key, '%s.gif' % path)
+def send_sample_gif(request, key, path):
+    directory = get_download_path(key)
+    if os.path.exists(directory):
+        filename = os.path.join(CACHE_DIR, key, '{}.gif'.format(path))
         if os.path.exists(filename):
             return send_raw_file(request, filename, attachment=False)
 
-        pngs = []
-        for f in os.listdir(get_download_path(obj.key)):
-            if f.endswith('.png') and f.startswith(path):
-                pngs.append(os.path.join(get_download_path(obj.key), f))
+        pngs = glob.glob(os.path.join(directory, '*.png'))
         if len(pngs) == 1:
             return send_raw_file(request, pngs[0], attachment=False)
-
-        create_cache_dir(obj.key)
-        command = 'convert -delay 200 -resize 500x {0} {1}'.format(' '.join(pngs), filename)
-        try:
-            subprocess.check_call(command.split())
-        except:
+        elif pngs:
+            create_cache_dir(key)
+            command = 'convert -delay 200 -resize 500x {0} {1}'.format(' '.join(pngs), filename)
             try:
+                subprocess.check_call(command.split())
                 subprocess.check_call(command.replace('_test-pic', '-pic').split())
-            except:
+            except subprocess.CalledProcessError:
                 return http.HttpResponseNotFound()
-        return send_raw_file(request, filename, attachment=False)
+            return send_raw_file(request, filename, attachment=False)
 
     return http.HttpResponseNotFound()
 
 
 def send_file(request, key, path):
-    obj = get_object_or_404(SecurePath, key=key)
-    document_root = obj.path
+    document_root = get_download_path(key)
+
     # Clean up given path to only allow serving files below document_root.
     path = posixpath.normpath(urllib.unquote(path))
-    path = path.lstrip('/')
-    newpath = ''
-    for part in path.split('/'):
-        if not part:
-            # Strip empty path components.
-            continue
-        drive, part = os.path.splitdrive(part)
-        head, part = os.path.split(part)
-        if part in (os.curdir, os.pardir):
-            # Strip '.' and '..' in path.
-            continue
-        newpath = os.path.join(newpath, part).replace('\\', '/')
-    if newpath and path != newpath:
-        return send_raw_file(request, newpath)
-    full_path = os.path.join(document_root, newpath)
-    if not os.path.exists(full_path) and os.path.exists("{}.gz".format(full_path)):
-        return send_uncompressed_file(request, obj, full_path)
-    return send_raw_file(request, full_path)
+    drive, path = os.path.splitdrive(path)  # Remove drive in case path is absolute
+    path = path.lstrip(os.path.sep)
+    full_path = os.path.join(document_root, path)
+    if not full_path.startswith(document_root):
+        return http.HttpResponseNotFound()
+
+    if os.path.exists('{}.gz'.format(full_path)):
+        return send_uncompressed_file(request, key, full_path)
+    elif os.path.exists(full_path):
+        return send_raw_file(request, full_path)
+
+    return http.HttpResponseNotFound()
 
 
 def send_archive(request, path, key=None):  # Add base parameter and another url
     if key:
         obj = get_object_or_404(SecurePath, key=key)
-        archive_dir = obj.path
+        target_path = obj.path
     else:
         paths = SecurePath.objects.filter(key__in=request.GET.getlist('urls[]')).values_list('path',flat=True)
-        archive_dir = os.path.commonprefix(paths)
-        archive_dir = archive_dir if archive_dir[-1] == '/' else '/'.join(archive_dir.split('/')[:-1])
-        if len([a for a in archive_dir.split(os.sep) if a]) < 3:
-            raise http.HttpResponseForbidden
+        target_path = os.path.commonprefix(paths)
 
-    if not os.path.exists(archive_dir):
-        archive_dir = "{}{}".format(ARCHIVE_ROOT, archive_dir)
+    target_path = target_path.rstrip(os.sep)
+    if len(target_path.split(os.sep)) < 4:
+        raise http.HttpResponseForbidden()
 
-    normpath = os.path.normpath(archive_dir)
+    archived_path = re.sub(ROOT_RE, ARCHIVE_ROOT, target_path)
+    source_path = os.path.normpath(target_path if os.path.exists(target_path) else archived_path)
 
-    if os.path.exists(archive_dir):
+    if os.path.exists(source_path):
         p = subprocess.Popen(
-            ['tar', '-czf', '-', os.path.basename(normpath)],
-            cwd=os.path.dirname(normpath),
+            ['tar', '-czf', '-', os.path.basename(source_path)],
+            cwd=os.path.dirname(source_path),
             stdout=subprocess.PIPE
         )
 
@@ -181,5 +172,5 @@ def send_archive(request, path, key=None):  # Add base parameter and another url
 
         return response
     else:
-        raise Http404
+        raise http.HttpResponseNotFound()
 
